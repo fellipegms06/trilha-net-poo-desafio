e5e3aad9fc6d31f4a7c83057543609dd9e01ea87
Fellipe Gomes <fellipe.gomes@ebserh.gov.br>
2025-07-30 17:05:37 -0300

    Implementa solução de problemas no código.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,22 @@
+using System.ComponentModel;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"""Instalando o aplicativo "{nomeApp}" no Nokia""");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "3897", modelo: "Nokia plus", iMEI: "1111111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "XS Max", iMEI:"222222222", memoria: 264);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
